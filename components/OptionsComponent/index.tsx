c5b9ff6b95fe0dae13d8337571f9be6fe3733d7d
JulioDev1 <juliocesargualberto617@gmail.com>
2025-02-15 17:02:13 -0300

    feature: selected row section

--- components/OptionsComponent/index.tsx
+++ components/OptionsComponent/index.tsx
@@ -1,9 +1,24 @@
+import Pencil from "@/public/Pencil";
 import IOptionsComponent from "../interfaces/IOptionsComponent";
+import Trash from "@/public/Trash";
 
-export default function OptionComponent(props:IOptionsComponent){
+export default function OptionComponent(props: IOptionsComponent) {
     return (
-        <div className={props.amount > 0  ? "w-11/12 flex items-center bg-white h-12 pl-4 font-semibold" : "hidden"}>
+        <div className={props.amount > 0 ? "w-11/12 flex items-center rounded justify-between p-3 bg-white h-12 pl-4 font-semibold"
+            : "hidden"}>
             {props.amount} rows selected
+            <div className="flex gap-3">
+                <span className="flex items-center gap-1 text-black">
+                    <Pencil />
+                    Edit
+                    <span className="text-black">( {props.amount} )</span>
+                </span>
+
+                <span className="flex items-center gap-1 text-red-600">
+                    <Trash />
+                    Delete <span className="text-red-600"> ( {props.amount} ) </span>
+                </span>
+            </div>
         </div>
     )
 }
